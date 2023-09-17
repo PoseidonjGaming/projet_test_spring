d94bb5cd23566823d7dba3c16a140bb55905a0b6
Lucas Magnier <73874300+PoseidonjGaming@users.noreply.github.com>
2023-09-17 11:57:25 +0200

    EpisodeService override toDto

--- src/main/java/fr/perso/springserie/service/EpisodeService.java
+++ src/main/java/fr/perso/springserie/service/EpisodeService.java
@@ -3,6 +3,8 @@ package fr.perso.springserie.service;
 import fr.perso.springserie.model.dto.EpisodeDTO;
 import fr.perso.springserie.model.entity.Episode;
 import fr.perso.springserie.repository.IEpisodeRepo;
+import fr.perso.springserie.repository.ISeasonRepo;
+import fr.perso.springserie.repository.ISeriesRepo;
 import fr.perso.springserie.service.interfaces.IEpisodeService;
 import org.springframework.stereotype.Service;
 
@@ -10,8 +12,13 @@ import java.util.List;
 
 @Service
 public class EpisodeService extends BaseService<Episode, EpisodeDTO> implements IEpisodeService {
-    public EpisodeService(IEpisodeRepo repository) {
+
+    private final ISeasonRepo seasonRepo;
+    private final ISeriesRepo seriesRepo;
+    public EpisodeService(IEpisodeRepo repository, ISeasonRepo seasonRepo, ISeriesRepo seriesRepo) {
         super(repository, EpisodeDTO.class, Episode.class);
+        this.seasonRepo = seasonRepo;
+        this.seriesRepo = seriesRepo;
     }
 
 
@@ -23,5 +30,22 @@ public class EpisodeService extends BaseService<Episode, EpisodeDTO> implements 
     @Override
     public List<EpisodeDTO> search(String term) {
         return null;
+    }
+
+    @Override
+    public EpisodeDTO toDTO(Episode entity) {
+        EpisodeDTO dto = super.toDTO(entity);
+        dto.setSeriesId(entity.getSeason().getSeries().getId());
+        return dto;
+    }
+
+    @Override
+    public void delete(int id) {
+        EpisodeDTO dto=getById(id);
+        super.delete(id);
+        seasonRepo.findById(dto.getSeasonId()).ifPresent(season -> {
+            if(season.getEpisodes().isEmpty())
+                seasonRepo.delete(season);
+        });
     }
 }
