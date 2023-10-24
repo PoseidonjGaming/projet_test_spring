2ab86191b465d189da66d4e14654ff4ec6641b7d
Lucas MAGNIER <lmagnier@dawan.fr>
2023-10-24 17:25:39 +0200

    test example matcher

--- src/main/java/fr/perso/springserie/service/ActorService.java
+++ src/main/java/fr/perso/springserie/service/ActorService.java
@@ -4,6 +4,8 @@ import fr.perso.springserie.model.dto.ActorDTO;
 import fr.perso.springserie.model.entity.Actor;
 import fr.perso.springserie.repository.IActorRepo;
 import fr.perso.springserie.service.interfaces.IActorService;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,5 +19,18 @@ public class ActorService extends BaseService<Actor, ActorDTO> implements IActor
     @Override
     public List<ActorDTO> search(String lastname) {
         return ((IActorRepo) repository).findByLastnameContaining(lastname).stream().map(this::toDTO).toList();
+    }
+
+    @Override
+    public List<ActorDTO> test(ActorDTO dto) {
+        List<Actor> entities=repository.findAll(Example.of(toEntity(dto),
+                ExampleMatcher.matchingAny()
+                        .withMatcher("firstname",
+                                ExampleMatcher.GenericPropertyMatchers.contains().caseSensitive())
+                        .withMatcher("lastname",
+                                ExampleMatcher.GenericPropertyMatchers.contains().caseSensitive())
+                        .withIgnoreNullValues()
+                        .withIgnorePaths("id")));
+        return toDTOList(entities);
     }
 }
--- src/main/java/fr/perso/springserie/service/interfaces/IActorService.java
+++ src/main/java/fr/perso/springserie/service/interfaces/IActorService.java
@@ -1,7 +1,11 @@
 package fr.perso.springserie.service.interfaces;
 
+import fr.perso.springserie.controller.ActorController;
 import fr.perso.springserie.model.dto.ActorDTO;
+
+import java.util.List;
 
 
 public interface IActorService extends IBaseService<ActorDTO> {
+    List<ActorDTO> test(ActorDTO dto);
 }
