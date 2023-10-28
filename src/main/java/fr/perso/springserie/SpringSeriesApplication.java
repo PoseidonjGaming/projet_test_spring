0762fbc28fd9a031c2c3c5c43f7bc71b63457dc5
Lucas Magnier <73874300+PoseidonjGaming@users.noreply.github.com>
2023-10-28 15:14:03 +0200

    rename app

--- src/main/java/fr/perso/springserie/SpringSeriesApplication.java
+++ src/main/java/fr/perso/springserie/SpringSeriesApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringserieApplication {
+public class SpringSeriesApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringserieApplication.class, args);
+        SpringApplication.run(SpringSeriesApplication.class, args);
     }
 
 }
