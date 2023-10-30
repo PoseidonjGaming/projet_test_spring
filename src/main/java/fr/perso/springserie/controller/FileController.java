3b0bc74e486c98fbd60f65b85b6b1e6e4365ed43
Lucas MAGNIER <lmagnier@dawan.fr>
2023-10-30 17:28:31 +0100

    up

--- src/main/java/fr/perso/springserie/controller/FileController.java
+++ src/main/java/fr/perso/springserie/controller/FileController.java
@@ -2,10 +2,8 @@ package fr.perso.springserie.controller;
 
 import fr.perso.springserie.service.interfaces.IFileService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -22,5 +20,23 @@ public class FileController {
     @PostMapping("/export")
     public ResponseEntity<?> test(@RequestBody List<Boolean> booleanList) {
         return service.writeExcel(booleanList);
+    }
+
+    @PostMapping("/save/file")
+    public ResponseEntity<?> saveFile(@RequestBody MultipartFile file) {
+        service.save(file);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/save/files")
+    public ResponseEntity<?> saveFiles(@RequestBody List<MultipartFile> files) {
+        service.saves(files);
+        return ResponseEntity.ok().build();
+    }
+
+
+    @GetMapping("/load")
+    public ResponseEntity<?> load(String filename) {
+        return service.load(filename);
     }
 }
