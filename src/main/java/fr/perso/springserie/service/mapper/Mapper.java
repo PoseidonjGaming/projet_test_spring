eb70aae8c015af4c5c6ce4c1d547af221514f9eb
Lucas MAGNIER <lmagnier@dawan.fr>
2023-12-06 17:28:35 +0100

    wip filter date

--- src/main/java/fr/perso/springserie/service/mapper/Mapper.java
+++ src/main/java/fr/perso/springserie/service/mapper/Mapper.java
@@ -53,7 +53,7 @@ public class Mapper implements IMapper {
             mapEntity(source, target, sourceField);
         } else if (sourceField.isAnnotationPresent(Embedded.class)) {
             mapEmbedded(source, target, sourceField);
-        } else {
+         } else {
             transfert(source, target, sourceField, target.getClass());
         }
         Arrays.stream(target.getClass().getDeclaredFields()).filter(field -> field.isAnnotationPresent(Embedded.class)).forEach(targetField -> {
